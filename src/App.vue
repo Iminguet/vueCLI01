4de02868e9fa3966a16aa354a217be0766b05771
Iminguet <i.minguet@hotmail.com>
2023-01-25 12:09:23 +0100

    componentes en local. style local

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
+    <TheHeader></TheHeader>
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -11,7 +12,12 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 export default {
+  // Javascript modern, al proporcionar un valor único al objeto, este interpreta ese valor como clave y como valor.
+  components: { TheHeader, BadgeList, UserInfo },
   data() {
     return {
       activeUser: {
